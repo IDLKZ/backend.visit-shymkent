cd95273e9b04bf76abd29f5697f25a321e660bf5
Misti <mistier.famous@gmail.com>
2021-07-19 11:35:27 +0600

    123

---
+++ resources/views/admin/user/create.blade.php
@@ -0,0 +1,119 @@
+
+@extends('layout.app')
+@section('content')
+
+    <div class="page-content">
+
+        <nav class="page-breadcrumb">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="#">{{__('admin.main')}}</a></li>
+                <li class="breadcrumb-item active" aria-current="page">{{__('admin.users')}}</li>
+            </ol>
+        </nav>
+
+        <div class="row">
+            <div class="col-md-12 grid-margin stretch-card">
+                <div class="card">
+                    <div class="card-body">
+                        <h6 class="card-title">{{__("admin.create")}}</h6>
+                        <form class="forms-sample" method="post" enctype="multipart/form-data" action="{{route('admin-user.store')}}">
+                            @csrf
+                            <div class="form-group" >
+                                <label>
+                                    {{__("admin.role_id")}}
+                                </label>
+                                <select class="js-example-basic-single w-100 select2-hidden-accessible" data-width="100%" data-select2-id="1" tabindex="-1" aria-hidden="true" name="role_id">
+                                    @if($roles->isNotEmpty())
+                                        @foreach($roles as $role)
+                                            <option value="{{$role->id}}">
+                                                {{$role->title}}
+                                            </option>
+                                        @endforeach
+                                    @endif
+                                </select>
+                                @error('role_id')
+                                <div class="invalid-feedback">
+                                    {{$message}}
+                                </div>
+                                @enderror
+                            </div>
+                            <div class="form-group">
+                                <label for="name">{{__('admin.name')}}</label>
+                                <input type="text" class="form-control  @error('name') is-invalid @enderror" id="name" name='name' autocomplete="off" placeholder="{{__('admin.name')}}" value="{{old('name')}}">
+                                @error('name')
+                                <div class="invalid-feedback">
+                                    {{$message}}
+                                </div>
+                                @enderror
+                            </div>
+                            <div class="form-group">
+                                <label for="phone">{{__('admin.phone')}}</label>
+                                <input type="text" class="form-control @error('phone') is-invalid @enderror" id="phone" name='phone' autocomplete="off" placeholder="{{__('admin.phone')}}" value="{{old('phone')}}">
+                                @error('phone')
+                                <div class="invalid-feedback">
+                                    {{$message}}
+                                </div>
+                                @enderror
+                            </div>
+                            <div class="form-group">
+                                <label for="email">{{__('admin.email')}}</label>
+                                <input type="email" class="form-control @error('email') is-invalid @enderror" id="email" name='email' autocomplete="off" placeholder="{{__('admin.email')}}" value="{{old('email')}}">
+                                @error('email')
+                                <div class="invalid-feedback">
+                                    {{$message}}
+                                </div>
+                                @enderror
+                            </div>
+                            <div class="form-group">
+                                <label for="exampleInputPassword1"> {{__("admin.password")}}</label>
+                                <input type="password" class="form-control @error('password') is-invalid @enderror" id="exampleInputPassword1" autocomplete="current-password" placeholder=" {{__("admin.password")}}" name="password">
+                                @error('password')
+                                <div class="invalid-feedback">
+                                    {{$message}}
+                                </div>
+                                @enderror
+                            </div>
+                            <div class="form-group">
+                                <label for="description">{{__('admin.description')}}</label>
+                                <input type="text" class="form-control @error('description') is-invalid @enderror" id="description" name='description' autocomplete="off" placeholder="{{__('admin.description')}}" value="{{old('description')}}">
+                                @error('description')
+                                <div class="invalid-feedback">
+                                    {{$message}}
+                                </div>
+                                @enderror
+                            </div>
+
+                            <div class="form-group">
+                                <label for="image">{{__('admin.image')}}</label>
+                                <input type="file" class="form-control @error('image') is-invalid @enderror" id="image" name='image'>
+                                @error('image')
+                                <div class="invalid-feedback">
+                                    {{$message}}
+                                </div>
+                                @enderror
+                            </div>
+                            <div class="form-group">
+                                <label for="status">{{__('admin.status')}}</label>
+                                <input id="status" type="checkbox"  data-toggle="toggle" data-on="{{__("admin.yes_status")}}" data-off="{{__("admin.not_status")}}" data-onstyle="success" data-offstyle="danger" name="status">
+
+                            </div>
+                            <div class="form-group">
+                                <label for="verified">{{__('admin.verified')}}</label>
+                                <input id="verified" type="checkbox"  data-toggle="toggle" data-on="{{__("admin.verified")}}" data-off="{{__("admin.not_verified")}}" data-onstyle="success" data-offstyle="danger" name="verified">
+
+                            </div>
+
+                            <button type="submit" class="btn btn-primary mr-2">{{__('admin.save')}}</button>
+                            <button class="btn btn-light">{{__('admin.cancel')}}</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+    </div>
+
+@endsection
+
+
