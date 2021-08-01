0a9830e2c4b2f268475eee376e167ab172224f75
Misti <mistier.famous@gmail.com>
2021-08-01 15:21:31 +0600

    123

---
+++ app/Http/Controllers/Api/EventController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Event;
+use Illuminate\Http\Request;
+
+class EventController extends Controller
+{
+    public function index(){
+        $events = Event::where("status",1)->count() >= 4  ? Event::where("status",1)->take(4) : Event::where("status",1)->get();
+        $events->load(["workdays","workdays.weekday"]);
+        return response()->json($events);
+
+
+
+    }
+}
