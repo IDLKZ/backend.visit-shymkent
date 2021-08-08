ceafcbb9d4604a4f899d660dd1796067d441d4e9
Misti <mistier.famous@gmail.com>
2021-08-08 19:36:09 +0600

    123

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -12,8 +12,10 @@ class EventController extends Controller
         $events = Event::where("status",1)->count() >= 4  ? Event::where("status",1)->take(4) : Event::where("status",1)->get();
         $events->load(["workdays","workdays.weekday"]);
         return response()->json($events);
+    }
 
-
-
+    public function event($alias){
+        $event = Event::with(['galleries'])->firstWhere("alias",$alias);
+        return response()->json($event);
     }
 }
