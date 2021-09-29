1812b66bd76cac0bc410bb225933f440547f9046
Misti <mistier.famous@gmail.com>
2021-09-29 22:50:14 +0600

    123

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('backUp:DB')->weeklyOn("1, '9:00'");
+        $schedule->command('backUp:DB')->everyMinute();
         $schedule->command('event:upload')->everyMinute();
     }
 
